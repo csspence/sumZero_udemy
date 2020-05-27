3d08abed38034fc25cbcef4562af02d01ed80018
Crew Spence <csspence123@gmail.com>
2020-05-27 06:53:42 -0500

    Created function stub for Udemy toy problem.

---
+++ sumZero_udemy.js
@@ -0,0 +1,15 @@
+/*
+From Udemy Algorithms Course
+
+Write a function called sumZero which accepts a sorted array of integers.
+The function should find the first pair where the sum is 0.
+Return an array that includes both values that sum to zero or undefined if a pair does not exit.
+
+sumZero([-3, -2, -1, 0, 1, 2, 3]) // [-3, 3]
+sumZero([-2, 0, 1, 3]) // undefined
+sumZero([1, 2, 3]) // undefined
+*/
+
+const sumZero = (array) => {
+  
+}
